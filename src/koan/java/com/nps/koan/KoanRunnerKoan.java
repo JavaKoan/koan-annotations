1384c628a253eb0ec10a4bcda46544aed65ab021
SmiddyPence <nicholas.p.smith@live.co.uk>
2014-02-04 09:04:12 +0000

    Added license

--- src/koan/java/com/nps/koan/KoanRunnerKoan.java
+++ src/koan/java/com/nps/koan/KoanRunnerKoan.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 Nicholas Smith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nps.koan;
 
 import com.nps.koan.fixture.annotation.Enlighten;
@@ -9,10 +25,6 @@ import org.junit.runner.RunWith;
 import static junit.framework.Assert.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-
-/**
- * Created by nicholas_smith on 02/01/14.
- */
 
 @RunWith(KoanRunner.class)
 public class KoanRunnerKoan {
--- src/main/java/com/nps/koan/fixture/io/KoanWriter.java
+++ src/main/java/com/nps/koan/fixture/io/KoanWriter.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 Nicholas Smith
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nps.koan.fixture.io;
 
 import java.io.BufferedWriter;
@@ -7,9 +23,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * Created by nicholas_smith on 06/01/14.
- */
 public class KoanWriter {
 
     private static final String KOAN_JAVA_PATH = "/src/koan/java/";
